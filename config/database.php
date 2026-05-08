cc7b5f5cf2e7e3221c1e51f1e127bf9a23aff90f
Adeel <adee@gmail.com>
2026-05-08 00:43:55 -0700

    chore: add more database environment variable fallbacks

--- config/database.php
+++ config/database.php
@@ -44,12 +44,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('MYSQL_URL') ?: env('DB_URL'),
-            'host' => env('MYSQLHOST') ?: env('DB_HOST', '127.0.0.1'),
-            'port' => env('MYSQLPORT') ?: env('DB_PORT', '3306'),
-            'database' => env('MYSQLDATABASE') ?: env('DB_DATABASE', 'laravel'),
-            'username' => env('MYSQLUSER') ?: env('DB_USERNAME', 'root'),
-            'password' => env('MYSQLPASSWORD') ?: env('DB_PASSWORD', ''),
+            'url' => env('MYSQL_URL') ?: env('DATABASE_URL') ?: env('DB_URL'),
+            'host' => env('MYSQLHOST') ?: env('MYSQL_HOST') ?: env('DB_HOST', '127.0.0.1'),
+            'port' => env('MYSQLPORT') ?: env('MYSQL_PORT') ?: env('DB_PORT', '3306'),
+            'database' => env('MYSQLDATABASE') ?: env('MYSQL_DATABASE') ?: env('DB_DATABASE', 'laravel'),
+            'username' => env('MYSQLUSER') ?: env('MYSQL_USER') ?: env('DB_USERNAME', 'root'),
+            'password' => env('MYSQLPASSWORD') ?: env('MYSQL_PASSWORD') ?: env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
